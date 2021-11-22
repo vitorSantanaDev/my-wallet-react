94e88c351aa93ae44656f4b9a2ae6711a95639cf
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-22 16:43:13 -0300

    🎨 Created component Content

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ContentWrapper } from "./styles";
+
+const Content: React.FC = () => {
+  return (
+    <ContentWrapper>
+      <h1>Content</h1>
+    </ContentWrapper>
+  );
+};
+
+export default Content;
---
+++ src/components/Content/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div `
+  grid-area: CT;
+  background-color: #14141C;
+`
