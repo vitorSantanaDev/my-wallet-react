3364e949aa80be006e77ac4b57b997c3e0560aa4
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-22 17:19:51 -0300

    ⚡️ Introduced the themes of our application

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,17 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: '#14141C',
+    secondary: '#171721',
+    tertiary: '#21212B',
+
+    white: '#F1F0F4',
+    black: '#090909',
+    gray: '#313139',
+
+    sucess: '#1ECA5F',
+    info: '#FBB759',
+    warning: '#E9766B'
+  }
+}
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,17 @@
+export default {
+  title: "light",
+
+  colors: {
+    primary: '#E5E5F1',
+    secondary: '#FDFDFF',
+    tertiary: '#F5F5FD',
+
+    white: '#090909',
+    black: '#F1F0F4',
+    gray: '#313139',
+
+    sucess: '#1ECA5F',
+    info: '#FBB759',
+    warning: '#E9766B'
+  }
+}
