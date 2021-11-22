6a2eb086d5e7d9f04a19b5588a4590841aacbbd3
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-22 16:43:55 -0300

    🎨 Created component MainHeader

---
+++ src/components/MainHeader/MainHeader.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { MainHeaderElement } from "./styles";
+
+const MainHeader: React.FC = () => {
+  return (
+    <MainHeaderElement>
+      <h1>Main Header</h1>
+    </MainHeaderElement>
+  );
+};
+
+export default MainHeader;
---
+++ src/components/MainHeader/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const MainHeaderElement = styled.header `
+  grid-area: MH;
+  background-color: #171721;
+`
