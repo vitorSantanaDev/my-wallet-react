2c0a7e637946e2768edb346b38e60254fd3e4260
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-22 16:42:50 -0300

    🎨 Created component Aside

---
+++ src/components/Aside/Aside.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AsideWrapper } from "./styles";
+
+const Aside: React.FC = () => {
+  return (
+    <AsideWrapper>
+      <h1>Aside</h1>
+    </AsideWrapper>
+  );
+};
+
+export default Aside;
---
+++ src/components/Aside/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const AsideWrapper = styled.aside `
+  grid-area: AS;
+  background-color: #171721;
+`
