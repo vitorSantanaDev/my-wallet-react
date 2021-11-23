3239acfd77a37492d1807f2a8601c8e01f86d09a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-23 17:07:43 -0300

    💄 Stylized MainHeader component

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,11 +1,23 @@
-import React from "react";
-import { MainHeaderElement } from "./styles";
+import React, { useMemo } from "react";
+import emojis from "../../utils/emojis";
+import Toggle from "../Toggle/Toggle";
+import * as S from "./styles";
 
 const MainHeader: React.FC = () => {
+
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length)
+    return emojis[index]
+  }, [])
+
   return (
-    <MainHeaderElement>
-      <h1>Main Header</h1>
-    </MainHeaderElement>
+    <S.MainHeaderElement>
+      <Toggle />
+      <S.ProfileWrapper>
+      <S.WelcomeWrapper>Olá, {emoji}</S.WelcomeWrapper>
+      <S.UserWrapper>Vitor Silva</S.UserWrapper>
+      </S.ProfileWrapper>
+    </S.MainHeaderElement>
   );
 };
 
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
-export const MainHeaderElement = styled.header `
+export const MainHeaderElement = styled.header`
   grid-area: MH;
+  display: flex;
+  padding: 0 16px;
+  align-items: center;
+  justify-content: space-between;
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.secondary};
-`
+  border-bottom: 1px dashed ${({ theme }) => theme.colors.gray};
+`;
+
+export const ProfileWrapper = styled.div`
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+export const WelcomeWrapper = styled.h3`
+  font-size: 18px;
+  font-weight: 400;
+  letter-spacing: 2px;
+`;
+
+export const UserWrapper = styled.span`
+  font-size: 14px;
+  letter-spacing: 2px;
+`;
