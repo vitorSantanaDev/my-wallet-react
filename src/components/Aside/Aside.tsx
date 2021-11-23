6b636e926f6de2c50ca0a9c542b8851cb4d1a73f
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-23 18:11:11 -0300

    🎨 Aside component structure completed

--- src/components/Aside/Aside.tsx
+++ src/components/Aside/Aside.tsx
@@ -1,10 +1,39 @@
 import React from "react";
 import { AsideWrapper } from "./styles";
+import Logo from "../../assets/images/logo.svg";
+import * as S from "./styles";
+import {
+  MdDashboard,
+  MdArrowDownward,
+  MdArrowUpward,
+  MdExitToApp,
+} from "react-icons/md";
 
 const Aside: React.FC = () => {
   return (
     <AsideWrapper>
-      <h1>Aside</h1>
+      <S.HeaderWrapper>
+        <S.Logo
+          src={Logo}
+          alt="Logo, apenas três circulos, um vermelho, um amarelo e um verde"
+        />
+      </S.HeaderWrapper>
+      <S.MenuWrapper>
+        <S.LinkMenu href="#">
+          <MdDashboard /> Dashboard
+        </S.LinkMenu>
+        <S.LinkMenu href="#">
+          <MdArrowUpward /> Appetizer
+        </S.LinkMenu>
+        <S.LinkMenu href="#">
+          <MdArrowDownward />
+          Exits
+        </S.LinkMenu>
+        <S.LinkMenu href="#">
+          <MdExitToApp />
+          To go out
+        </S.LinkMenu>
+      </S.MenuWrapper>
     </AsideWrapper>
   );
 };
