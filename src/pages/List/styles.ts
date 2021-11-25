2c59eb9002fd145b5648bd443df0be42b7d500ec
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-25 17:18:52 -0300

    💄 Some added styles

---
+++ src/pages/List/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const ListWrapper = styled.div``;
+
+export const ListContent = styled.div``;
+
+export const Filters = styled.div`
+  width: 100%;
+  display: flex;
+  margin-bottom: 32px;
+  justify-content: center;
+
+  .tagFilter {
+    margin: 0 10px;
+    font-size: 18px;
+    background: none;
+    font-weight: 500;
+    transition: opacity ease-in-out 0.3s;
+    color: ${({ theme }) => theme.colors.white};
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &.recurrent {
+      &::after {
+        content: "";
+        width: 55px;
+        display: block;
+        margin: 8px auto;
+        border-radius: 4px;
+        border-bottom: 10px solid ${({ theme }) => theme.colors.warning};
+      }
+    }
+
+    &.eventual {
+      &::after {
+        content: "";
+        width: 55px;
+        display: block;
+        margin: 8px auto;
+        border-radius: 4px;
+        border-bottom: 10px solid ${({ theme }) => theme.colors.sucess};
+      }
+    }
+  }
+`;
