5ed1c824cc6ca0a437f21ecc5870d5bd1f7ab96b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-11-25 17:16:36 -0300

    💄 Height changes, and scroll styling

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -3,6 +3,21 @@ import styled from "styled-components";
 export const ContentWrapper = styled.div `
   grid-area: CT;
   padding: 24px;
+  overflow-y: scroll;
+  height: calc(100vh - 70px);
   color: ${({ theme }) => theme.colors.white};
   background-color: ${({ theme }) => theme.colors.primary};
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: ${({ theme }) => theme.colors.secondary};
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.tertiary};
+  }
 `
