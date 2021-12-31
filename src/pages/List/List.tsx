c41a2e095b1f982b4728b0e1092c97332e42dcfa
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-31 13:42:22 -0300

    ✨ Added filtering data according to URL, and rendering to screen.

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,12 +1,24 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
 import HistoryCard from "../../components/HistroyCard/HistoryCard";
 import Select from "../../components/Select/Select";
-import dark from "../../styles/themes/dark"
+import dark from "../../styles/themes/dark";
+import gains from "../../repositories/gains";
+import expenses from "../../repositories/expenses";
 import * as S from "./styles";
 
+interface IData {
+  id: string;
+  description: string;
+  amountFormated: string;
+  frequency: string;
+  dataFormated: string;
+  tagColor: string;
+}
+
 const List: React.FC = () => {
+  const [data, setData] = useState<IData[]>([]);
   const { type } = useParams();
 
   const titleAndLineColor = useMemo(() => {
@@ -14,8 +26,28 @@ const List: React.FC = () => {
       ? { title: "Appetizer", lineColor: dark.colors.sucess }
       : { title: "Exits", lineColor: dark.colors.warning };
   }, [type]);
+  const { title, lineColor } = titleAndLineColor;
 
-  const { title, lineColor } = titleAndLineColor
+  const listData = useMemo(() => {
+    return type === "entry-balance" ? gains : expenses;
+  }, [type]);
+
+  useEffect(() => {
+    const response = listData.map((data) => {
+      return {
+        id: String(Math.random() * listData.length),
+        description: data.description,
+        amountFormated: data.amount,
+        frequency: data.frequency,
+        dataFormated: data.date,
+        tagColor:
+          data.frequency === "recorrente"
+            ? dark.colors.sucess
+            : dark.colors.info,
+      };
+    });
+    setData(response);
+  }, [listData]);
 
   const monthly = [
     { value: 11, label: "Novembro" },
@@ -48,12 +80,15 @@ const List: React.FC = () => {
       </S.Filters>
 
       <S.ListContent>
-        <HistoryCard
-          tagColor="#1ECA5F"
-          title="Internet"
-          subTitle="20/11/20"
-          amount="R$ 50,00"
-        />
+        {data.map((item, index) => (
+          <HistoryCard
+            key={index}
+            tagColor={item.tagColor}
+            title={item.description}
+            subTitle={item.dataFormated}
+            amount={item.amountFormated}
+          />
+        ))}
       </S.ListContent>
     </S.ListWrapper>
   );
