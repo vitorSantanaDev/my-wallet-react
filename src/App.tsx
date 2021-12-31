eb326e19f7ca5cf7f050d7e3c36b7d8a8ef5525b
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-31 10:28:45 -0300

    🚚 Added the Routes in the App  component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import Layout from './components/Layout/Layout';
-import GlobalStyle from './styles/global';
-import { ThemeProvider } from "styled-components"
-import dark from './styles/themes/dark';
-import Dashboard from './pages/Dashboard/Dashboard';
+import React from "react";
+import GlobalStyle from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import dark from "./styles/themes/dark";
+import Routes from "./routes";
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={dark}>
       <GlobalStyle />
-      <Layout>
-        <Dashboard />
-      </Layout>
+      <Routes />
     </ThemeProvider>
   );
-}
+};
 
 export default App;
