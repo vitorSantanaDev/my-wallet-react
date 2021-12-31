4347b99d91afc8632081bc1ea66ac3259fe3179e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-31 10:23:58 -0300

    🎨 Added component Select in the interface.

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
+import Select from "../../components/Select/Select";
 import * as S from "./styles"
 
 const Dashboard: React.FC = () => {
+
+  const options = [{value: "Vitor", label: "Vitor"}, {value: "Cassia", label: "Cassia"}]
+
   return (
     <S.DashboardWrapper>
-      <ContentHeader />
+      <ContentHeader title="Dashboard" lineColor="#E9766B">
+        <Select options={options}/>
+      </ContentHeader>
     </S.DashboardWrapper>
   );
 };
