3de7eed708e79335ac1dabda59eb3f8a87d67b2e
vitorSantanaDev <vitorsantana.developer@gmail.com>
2021-12-31 10:21:43 -0300

    🎨 Organizing the code.

--- src/styles/themes/dark.ts
+++ src/styles/themes/dark.ts
@@ -2,17 +2,17 @@ export default {
   title: "dark",
 
   colors: {
-    primary: '#14141C',
-    secondary: '#171721',
-    tertiary: '#21212B',
+    primary: "#14141C",
+    secondary: "#171721",
+    tertiary: "#21212B",
 
-    white: '#F1F0F4',
-    black: '#090909',
-    gray: '#313139',
+    white: "#F1F0F4",
+    black: "#090909",
+    gray: "#313139",
 
-    sucess: '#1ECA5F',
-    info: '#FBB759',
-    warning: '#E9766B',
-    link: '#9F9F9E',
-  }
-}
+    sucess: "#1ECA5F",
+    info: "#FBB759",
+    warning: "#E9766B",
+    link: "#9F9F9E",
+  },
+};
