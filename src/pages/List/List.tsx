a17e0c6f0411cd4ff48deab9ae51eafa26466c87
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-01-02 12:21:02 -0300

    ⚡️ Making filtering months and years more perfomatic.

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,5 +1,6 @@
 import React, { useMemo, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { v4 as uuidv4 } from 'uuid';
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
 import HistoryCard from "../../components/HistroyCard/HistoryCard";
 import Select from "../../components/Select/Select";
@@ -8,6 +9,7 @@ import gains from "../../repositories/gains";
 import expenses from "../../repositories/expenses";
 import formatCurrecy from "../../utils/formatCurrent";
 import formatDate from "../../utils/formatDate";
+import listOfmonths from "../../utils/months";
 import * as S from "./styles";
 
 interface IData {
@@ -52,7 +54,7 @@ const List: React.FC = () => {
 
     const formattedData = filteredDate.map((data) => {
       return {
-        id: String(Math.random() * listData.length),
+        id: uuidv4(),
         description: data.description,
         amountFormated: formatCurrecy(Number(data.amount)),
         frequency: data.frequency,
@@ -64,38 +66,36 @@ const List: React.FC = () => {
       };
     });
 
-    setData(formattedData)
+    setData(formattedData);
   }, [listData, monthSelected, yearSelected]);
 
-  const monthly = [
-    { value: 1, label: "Janeiro" },
-    { value: 2, label: "Fevereiro" },
-    { value: 3, label: "Março" },
-    { value: 4, label: "Abril" },
-    { value: 5, label: "Maio" },
-    { value: 6, label: "Junho" },
-    { value: 7, label: "Julho" },
-    { value: 8, label: "Agosto" },
-    { value: 9, label: "Setembro" },
-    { value: 10, label: "Outubro" },
-    { value: 11, label: "Novembro" },
-    { value: 12, label: "Dezembro" },
-  ];
+  const monthly = useMemo(() => {
+    return listOfmonths.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month,
+      };
+    });
+  }, []);
 
-  const years = [
-    { value: 2010, label: 2010 },
-    { value: 2011, label: 2011 },
-    { value: 2013, label: 2013 },
-    { value: 2014, label: 2014 },
-    { value: 2015, label: 2015 },
-    { value: 2016, label: 2016 },
-    { value: 2017, label: 2017 },
-    { value: 2018, label: 2018 },
-    { value: 2019, label: 2019 },
-    { value: 2020, label: 2020 },
-    { value: 2021, label: 2021 },
-    { value: 2022, label: 2022 }
-  ];
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    listData.forEach((item) => {
+      const date = new Date(item.date);
+      const yaer = date.getFullYear();
+
+      if (!uniqueYears.includes(yaer)) {
+        uniqueYears.push(yaer);
+      }
+    });
+    return uniqueYears.map((year) => {
+      return {
+        value: year,
+        label: year,
+      };
+    });
+  }, [listData]);
 
   return (
     <S.ListWrapper>
