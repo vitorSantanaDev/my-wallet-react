7ef614564e6e78465ca87d9b744e6b3264cfa1dd
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-01-07 00:05:06 -0300

    💄 Added new styles

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
 export const DashboardWrapper = styled.div ``
+
+export const ContentDashboard = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
