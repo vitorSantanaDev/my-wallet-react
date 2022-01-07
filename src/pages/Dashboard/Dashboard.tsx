1982a6c640ff8a3ef645a4a30208f0300ad9881a
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-01-07 00:04:52 -0300

    🎨 Making use of the component WalletBox

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,16 +1,20 @@
 import React, { useState, useMemo } from "react";
 import ContentHeader from "../../components/ContentHeader/ContentHeader";
 import Select from "../../components/Select/Select";
+import WalletBox from "../../components/WalletBox/WalletBox";
 import expenses from "../../repositories/expenses";
 import gains from "../../repositories/gains";
-import listOfMonths from "../../utils/months"
-import * as S from "./styles"
+import listOfMonths from "../../utils/months";
+import dark from "../../styles/themes/dark";
+import * as S from "./styles";
 
 const Dashboard: React.FC = () => {
   const [monthSelected, setMonthSelected] = useState<number>(
     new Date().getMonth() + 1
   );
-  const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
+  const [yearSelected, setYearSelected] = useState<number>(
+    new Date().getFullYear()
+  );
   const monthly = useMemo(() => {
     return listOfMonths.map((month, index) => {
       return {
@@ -41,28 +45,26 @@ const Dashboard: React.FC = () => {
 
   const handleMonthSelected = (month: string) => {
     try {
-      const parseMonth = Number(month)
-      setMonthSelected(parseMonth)
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch (error) {
+      throw new Error("Invalid month value. Is accept 0 - 24.");
     }
-    catch(error) {
-      throw new Error('Invalid month value. Is accept 0 - 24.')
-    }
-  }
+  };
 
   const handleYearSelected = (year: string) => {
     try {
-      const parseYear = Number(year)
-      setYearSelected(parseYear)
-    }  
-    catch(error) {
-      throw new Error('Invalid year value. Is accept integer numbers.')
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch (error) {
+      throw new Error("Invalid year value. Is accept integer numbers.");
     }
-  }
+  };
 
   return (
     <S.DashboardWrapper>
       <ContentHeader title="Dashboard" lineColor="#23abeb">
-      <Select
+        <Select
           defaultValue={monthSelected}
           options={monthly}
           onChange={({ target }) => handleMonthSelected(target.value)}
@@ -73,6 +75,29 @@ const Dashboard: React.FC = () => {
           onChange={({ target }) => handleYearSelected(target.value)}
         />
       </ContentHeader>
+      <S.ContentDashboard>
+        <WalletBox
+          color={dark.colors.sucess}
+          icon="Money"
+          title="Saldo"
+          amount={150}
+          footerLabel="Atualizado com base nas entradas e saídas."
+        />
+        <WalletBox
+          color={dark.colors.info}
+          icon="Arrow up"
+          title="Entradas"
+          amount={5000.00}
+          footerLabel="Atualizado com base nas entradas e saídas."
+        />
+        <WalletBox
+          color={dark.colors.warning}
+          icon="Arrow down"
+          title="Saídas"
+          amount={4800.00}
+          footerLabel="Atualizado com base nas entradas e saídas."
+        />
+      </S.ContentDashboard>
     </S.DashboardWrapper>
   );
 };
