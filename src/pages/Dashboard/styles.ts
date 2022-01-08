029e364918b60fdfcefc6eef05d59822e279778d
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-01-07 23:50:01 -0300

    💄 Added some style change

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const DashboardWrapper = styled.div ``
 
 export const ContentDashboard = styled.div`
-  display: flex;
-  justify-content: space-between;
+display: flex;
+flex-wrap: wrap;
+align-items: center;
+justify-content: space-between;
 `
