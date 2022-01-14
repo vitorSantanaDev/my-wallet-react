34127a085e14ff7aaef2093dd50deb576b504a32
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-01-14 01:04:50 -0300

    🚚 Doing a check to redeem a route from the "logged" value

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { useAuth } from "../hooks/auth";
 import AppRoutes from "./app.routes";
+import AuthRoutes from "./auth.routes";
 
 const Routes: React.FC = () => {
+  const { logged } = useAuth();
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+    <BrowserRouter>{logged ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 };
 
